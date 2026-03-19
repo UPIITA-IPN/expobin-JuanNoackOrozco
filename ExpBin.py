c242e1185f6e1c558cf0b5266e1db177d62b71fc
JuanNoackOrozco <jramirezo1200@alumno.ipn.mx>
2026-03-19 15:01:45 +0000

    Update

---
+++ ExpBin.py
@@ -0,0 +1,15 @@
+def ExpBin(M,e,n):
+    e_bin = bin(e)[2:]
+    #print("e en binario:", e_bin)
+    if e_bin[0] == '1': C = M
+    else: C = 1
+    for i in range(1, len(e_bin)):
+        C = (C * C) % n
+        if e_bin[i] == '1': C = (C * M) % n
+    return C
+
+#print(ExpBin(3,381,10))
+#print(ExpBin(3,34505,100)) # 3^34505 mod 100 = 43
+#print(ExpBin(2,342405,101)) # 2^342405 mod 101 = 32
+#print(ExpBin(7,32945718283912847,26)) # 7^32945718283912847 mod 26 = 7
+print(ExpBin(2,1234,789)) # 2^1234 mod 789 = 481
